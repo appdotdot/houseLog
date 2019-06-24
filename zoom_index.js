90f5e26af2036ebffc9dee933ac8bfe46b0f6296
yuqi <nickiyu@163.com>
2019-06-25 01:54:40 +0800

    init

---
+++ zoom_index.js
@@ -0,0 +1,94 @@
+dictwords = ["2019年2月24日 0 18 144",
+	"top 0 10 16 1 10 8 2 10 4 3 10 8 4 10 16 5 10 16 6 10 16 7 10 8 8 10 8 9 10 8",
+	"next 0 10 16 1 10 4 2 10 2 3 10 4 4 10 8 5 10 8 6 10 16 7 10 8 8 10 4",
+	"这是开发商通知收房后第一次去验房 0 10 8",
+	"当时发现存在多处漏水 0 10 8",
+	"补漏的地方 0 10 8",
+	"并提交整改单 0 10 8",
+	"景观阳台漏水 0 10 8",
+	"补漏情况 0 26 12",
+	"阁楼屋顶漏水 0 10 8",
+	"阁楼侧墙漏水 0 10 4",
+	"阁楼线管漏水情况 0 10 2",
+	"另外还有发现楼梯道墙壁渗水 0 10 2",
+	"18层过道有渗水痕迹 0 10 2",
+	"问题有写入整改单 0 10 1",
+	"但未拍照留存 0 10 1",
+	"2019224.htm 0 6 64",
+	"2019年2月26日 1 18 136",
+	"previous 1 10 4 2 10 2 3 10 4 4 10 8 5 10 16 6 10 16 7 10 8 8 10 8 9 10 8",
+	"景观阳台处理 1 10 2",
+	"厨房烟道发现漏水情况 1 10 2",
+	"阁楼屋顶 1 10 2 3 10 2 4 10 4 5 10 2 6 10 4",
+	"2019226.htm 1 6 64",
+	"2019年3月30日 2 18 136",
+	"2019330.htm 2 6 64",
+	"2019年4月27日 3 18 136",
+	"楼梯间 3 10 2 4 10 8 5 10 8 6 10 8 7 10 4 9 10 4",
+	"阁楼侧墙 3 10 1 4 10 2 5 10 2 6 10 4 7 10 4 8 10 4",
+	"2019427.htm 3 6 64",
+	"2019年4月30日 4 18 144",
+	"厨房烟道 4 10 2 5 10 4 6 10 8 7 10 2 8 10 2 9 10 2",
+	"2019430.htm 4 6 64",
+	"2019年5月18日 5 18 144",
+	"景观阳台 5 10 4 6 10 2 7 10 2 8 10 1 9 10 2",
+	"阁楼线管 5 10 2 6 10 2 7 10 2 8 10 2 9 10 2",
+	"2019518.htm 5 6 64",
+	"2019年5月25日 6 18 144",
+	"2019525.htm 6 6 64",
+	"2019年6月10日 7 18 144",
+	"11日 7 18 144",
+	"2019610 7 6 64",
+	"11.htm 7 6 64",
+	"2019年6月19日 8 18 144",
+	"2019619.htm 8 6 64",
+	"2019年6月22日 9 18 144",
+	"入户门 9 10 4",
+	"阁楼侧墙和地面 9 10 4",
+	"2019622.htm 9 6 64"];
+skipwords = ["and,or,the,it,is,an,on,we,us,to,of,"];
+var STR_FORM_SEARCHFOR = "Search for:";
+var STR_FORM_SUBMIT_BUTTON = "Submit";
+var STR_FORM_RESULTS_PER_PAGE = "Results per page:";
+var STR_FORM_CATEGORY = "Category:";
+var STR_FORM_CATEGORY_ALL = "All";
+var STR_FORM_MATCH = "Match:";
+var STR_FORM_ANY_SEARCH_WORDS = "any search words";
+var STR_FORM_ALL_SEARCH_WORDS = "all search words";
+var STR_NO_QUERY = "No search query entered.";
+var STR_RESULTS_FOR = "Search results for:";
+var STR_RESULTS_IN_ALL_CATEGORIES = "in all categories";
+var STR_RESULTS_IN_CATEGORY = "in category";
+var STR_POWEREDBY = "Search powered by";
+var STR_NO_RESULTS = "No results";
+var STR_RESULT = "result";
+var STR_RESULTS = "results";
+var STR_PHRASE_CONTAINS_COMMON_WORDS = "Your search query contained too many common words to return the entire set of results available. Please try again with a more specific query for better results.";
+var STR_SKIPPED_FOLLOWING_WORDS = "The following word(s) are in the skip word list and have been omitted from your search:";
+var STR_SKIPPED_PHRASE = "Note that you can not search for exact phrases beginning with a skipped word";
+var STR_SUMMARY_NO_RESULTS_FOUND = "No results found.";
+var STR_SUMMARY_FOUND_CONTAINING_ALL_TERMS = "found containing all search terms.";
+var STR_SUMMARY_FOUND_CONTAINING_SOME_TERMS = "found containing some search terms.";
+var STR_SUMMARY_FOUND = "found.";
+var STR_PAGES_OF_RESULTS = "pages of results.";
+var STR_MORETHAN = "More than";
+var STR_POSSIBLY_GET_MORE_RESULTS = "You can possibly get more results searching for";
+var STR_ANY_OF_TERMS = "any of the terms";
+var STR_ALL_CATS = "all categories";
+var STR_CAT_SUMMARY = "Refine your search by category:";
+var STR_DIDYOUMEAN = "Did you mean:";
+var STR_OR = "or";
+var STR_RECOMMENDED = "Recommended links";
+var STR_SORTEDBY_RELEVANCE = "Sorted by relevance";
+var STR_SORTBY_RELEVANCE = "Sort by relevance";
+var STR_SORTBY_DATE = "Sort by date";
+var STR_SORTEDBY_DATE = "Sorted by date";
+var STR_RESULT_TERMS_MATCHED = "Terms matched: ";
+var STR_RESULT_SCORE = "Score: ";
+var STR_RESULT_URL = "URL:";
+var STR_RESULT_PAGES = "Result Pages:";
+var STR_RESULT_PAGES_PREVIOUS = "Previous";
+var STR_RESULT_PAGES_NEXT = "Next";
+var STR_SEARCH_TOOK = "Search took";
+var STR_SECONDS = "seconds";
+var STR_MAX_RESULTS = "You have requested more results than served per query. Please try again with a more precise query.";
